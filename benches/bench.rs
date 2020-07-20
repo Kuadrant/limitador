4a0510bd963de65339b97e56f1ff9539b8dd7aea
David Ortiz <z.david.ortiz@gmail.com>
2020-07-20 14:07:06 +0200

    benches: use criterion

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,54 +1,49 @@
-#![feature(test)]
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-extern crate test;
+use limitador::limit::Limit;
+use limitador::RateLimiter;
+use std::collections::HashMap;
 
-#[cfg(test)]
-mod tests {
-    use limitador::limit::Limit;
-    use limitador::RateLimiter;
-    use std::collections::HashMap;
-    use test::{black_box, Bencher};
+fn bench_is_rate_limited(c: &mut Criterion) {
+    let rate_limiter = test_rate_limiter();
+    let values = test_values();
 
-    #[bench]
-    fn bench_is_rate_limited(b: &mut Bencher) {
-        let rate_limiter = test_rate_limiter();
-        let values = test_values();
-
-        b.iter(|| {
-            black_box(rate_limiter.is_rate_limited(&values, 1).unwrap());
-        });
-    }
-
-    #[bench]
-    fn bench_update_counters(b: &mut Bencher) {
-        let mut rate_limiter = test_rate_limiter();
-        let values = test_values();
-
-        b.iter(|| {
-            black_box(rate_limiter.update_counters(&values, 1).unwrap());
-        });
-    }
-
-    fn test_rate_limiter() -> RateLimiter {
-        let limit = Limit::new(
-            "test_namespace",
-            100_000_000_000,
-            60,
-            vec!["req.method == GET"],
-            vec!["req.method", "app_id"],
-        );
-
-        let mut rate_limiter = RateLimiter::default();
-        rate_limiter.add_limit(limit).unwrap();
-
-        rate_limiter
-    }
-
-    fn test_values() -> HashMap<String, String> {
-        let mut values: HashMap<String, String> = HashMap::new();
-        values.insert("namespace".to_string(), "test_namespace".to_string());
-        values.insert("req.method".to_string(), "GET".to_string());
-        values.insert("app_id".to_string(), "test_app_id".to_string());
-        values
-    }
+    c.bench_function("is rate limited", |b| {
+        b.iter(|| black_box(rate_limiter.is_rate_limited(&values, 1).unwrap()))
+    });
 }
+
+fn bench_update_counters(c: &mut Criterion) {
+    let mut rate_limiter = test_rate_limiter();
+    let values = test_values();
+
+    c.bench_function("update counters", |b| {
+        b.iter(|| black_box(rate_limiter.update_counters(&values, 1).unwrap()));
+    });
+}
+
+fn test_rate_limiter() -> RateLimiter {
+    let limit = Limit::new(
+        "test_namespace",
+        100_000_000_000,
+        60,
+        vec!["req.method == GET"],
+        vec!["req.method", "app_id"],
+    );
+
+    let mut rate_limiter = RateLimiter::default();
+    rate_limiter.add_limit(limit).unwrap();
+
+    rate_limiter
+}
+
+fn test_values() -> HashMap<String, String> {
+    let mut values: HashMap<String, String> = HashMap::new();
+    values.insert("namespace".to_string(), "test_namespace".to_string());
+    values.insert("req.method".to_string(), "GET".to_string());
+    values.insert("app_id".to_string(), "test_app_id".to_string());
+    values
+}
+
+criterion_group!(benches, bench_is_rate_limited, bench_update_counters);
+criterion_main!(benches);
