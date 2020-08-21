d3aef132aafa355e07fd7dba0469f57595d56ba8
David Ortiz <z.david.ortiz@gmail.com>
2020-08-21 11:29:04 +0200

    storage: add async version

--- limitador/src/storage/mod.rs
+++ limitador/src/storage/mod.rs
@@ -3,6 +3,7 @@ use ::redis::RedisError;
 
 use crate::counter::Counter;
 use crate::limit::Limit;
+use async_trait::async_trait;
 use std::collections::HashSet;
 use thiserror::Error;
 
@@ -25,6 +26,22 @@ pub trait Storage: Sync + Send {
         delta: i64,
     ) -> Result<bool, StorageErr>;
     fn get_counters(&self, namespace: &str) -> Result<HashSet<Counter>, StorageErr>;
+}
+
+#[async_trait]
+pub trait AsyncStorage: Sync + Send {
+    async fn add_limit(&self, limit: &Limit) -> Result<(), StorageErr>;
+    async fn get_limits(&self, namespace: &str) -> Result<HashSet<Limit>, StorageErr>;
+    async fn delete_limit(&self, limit: &Limit) -> Result<(), StorageErr>;
+    async fn delete_limits(&self, namespace: &str) -> Result<(), StorageErr>;
+    async fn is_within_limits(&self, counter: &Counter, delta: i64) -> Result<bool, StorageErr>;
+    async fn update_counter(&self, counter: &Counter, delta: i64) -> Result<(), StorageErr>;
+    async fn check_and_update(
+        &self,
+        counters: &HashSet<&Counter>,
+        delta: i64,
+    ) -> Result<bool, StorageErr>;
+    async fn get_counters(&self, namespace: &str) -> Result<HashSet<Counter>, StorageErr>;
 }
 
 #[derive(Error, Debug)]
