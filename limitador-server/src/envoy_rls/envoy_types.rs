66c8142c2bb93de7248eafb8373a2c94eefd7fc6
David Ortiz <z.david.ortiz@gmail.com>
2020-12-09 16:41:54 +0100

    envoy-rls/envoy_types: use #[path]
    Instead of include! because it's not compatible with rust-analyzer.
    Notice also that one of the include!'s was not necessary.

--- limitador-server/src/envoy_rls/envoy_types.rs
+++ limitador-server/src/envoy_rls/envoy_types.rs
@@ -1,35 +1,38 @@
+#[path = "protobufs"]
 pub mod envoy {
+    #[path = "."]
     pub mod config {
+        #[path = "."]
         pub mod core {
-            pub mod v3 {
-                include!("protobufs/envoy.config.core.v3.rs");
-            }
+            #[path = "envoy.config.core.v3.rs"]
+            pub mod v3;
         }
     }
 
+    #[path = "."]
     pub mod extensions {
+        #[path = "."]
         pub mod common {
+            #[path = "."]
             pub mod ratelimit {
-                pub mod v3 {
-                    include!("protobufs/envoy.extensions.common.ratelimit.v3.rs");
-                }
+                #[path = "envoy.extensions.common.ratelimit.v3.rs"]
+                pub mod v3;
             }
         }
     }
 
+    #[path = "."]
     pub mod r#type {
-        include!("protobufs/envoy.r#type.rs");
-
-        pub mod v3 {
-            include!("protobufs/envoy.r#type.v3.rs");
-        }
+        #[path = "envoy.r#type.v3.rs"]
+        pub mod v3;
     }
 
+    #[path = "."]
     pub mod service {
+        #[path = "."]
         pub mod ratelimit {
-            pub mod v3 {
-                include!("protobufs/envoy.service.ratelimit.v3.rs");
-            }
+            #[path = "envoy.service.ratelimit.v3.rs"]
+            pub mod v3;
         }
     }
 }
