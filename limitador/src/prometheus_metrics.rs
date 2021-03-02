724aa9d4d0d1de961109b9070aa1d08fb041038e
David Ortiz <z.david.ortiz@gmail.com>
2021-03-02 18:45:35 +0100

    prometheus_metrics: simplify by not using statics
    Easier to test and easier to reason about.

--- limitador/src/prometheus_metrics.rs
+++ limitador/src/prometheus_metrics.rs
@@ -1,60 +1,64 @@
 use crate::limit::Namespace;
 use prometheus::{Encoder, IntCounterVec, IntGauge, Opts, Registry, TextEncoder};
-use std::sync::Once;
 
 const NAMESPACE_LABEL: &str = "limitador_namespace";
 
-lazy_static! {
-    static ref REGISTRY: Registry = Registry::new();
-
-    static ref AUTHORIZED_CALLS: IntCounterVec = IntCounterVec::new(
-        Opts::new("authorized_calls", "Authorized calls"),
-        &[NAMESPACE_LABEL]
-    )
-    .unwrap();
-
-    static ref LIMITED_CALLS: IntCounterVec = IntCounterVec::new(
-        Opts::new("limited_calls", "Limited calls"),
-        &[NAMESPACE_LABEL],
-    )
-    .unwrap();
-
-    // This can be used as a simple health check
-    static ref LIMITADOR_UP: IntGauge =
-        IntGauge::new("limitador_up", "Limitador is running").unwrap();
+pub struct PrometheusMetrics {
+    registry: Registry,
+    authorized_calls: IntCounterVec,
+    limited_calls: IntCounterVec,
 }
 
-static REGISTER_METRICS: Once = Once::new();
+impl PrometheusMetrics {
+    pub fn new() -> Self {
+        let labels = vec![NAMESPACE_LABEL];
 
-pub fn register_metrics() {
-    REGISTER_METRICS.call_once(|| {
-        REGISTRY
-            .register(Box::new(AUTHORIZED_CALLS.clone()))
+        let authorized_calls =
+            IntCounterVec::new(Opts::new("authorized_calls", "Authorized calls"), &labels).unwrap();
+
+        let limited_calls =
+            IntCounterVec::new(Opts::new("limited_calls", "Limited calls"), &labels).unwrap();
+
+        // Can be used as a simple health check
+        let limitador_up = IntGauge::new("limitador_up", "Limitador is running").unwrap();
+
+        let registry = Registry::new();
+
+        registry
+            .register(Box::new(authorized_calls.clone()))
             .unwrap();
 
-        REGISTRY.register(Box::new(LIMITED_CALLS.clone())).unwrap();
-        REGISTRY.register(Box::new(LIMITADOR_UP.clone())).unwrap();
+        registry.register(Box::new(limited_calls.clone())).unwrap();
+        registry.register(Box::new(limitador_up.clone())).unwrap();
 
-        LIMITADOR_UP.set(1);
-    })
-}
+        limitador_up.set(1);
 
-pub fn incr_authorized_calls(namespace: &Namespace) {
-    AUTHORIZED_CALLS
-        .with_label_values(&[namespace.as_ref()])
-        .inc();
-}
+        Self {
+            registry,
+            authorized_calls,
+            limited_calls,
+        }
+    }
 
-pub fn incr_limited_calls(namespace: &Namespace) {
-    LIMITED_CALLS.with_label_values(&[namespace.as_ref()]).inc();
-}
+    pub fn incr_authorized_calls(&self, namespace: &Namespace) {
+        self.authorized_calls
+            .with_label_values(&[namespace.as_ref()])
+            .inc();
+    }
 
-pub fn gather_metrics() -> String {
-    let mut buffer = Vec::new();
+    pub fn incr_limited_calls(&self, namespace: &Namespace) {
+        self.limited_calls
+            .with_label_values(&[namespace.as_ref()])
+            .inc();
+    }
 
-    TextEncoder::new()
-        .encode(&REGISTRY.gather(), &mut buffer)
-        .unwrap();
+    pub fn gather_metrics(&self) -> String {
+        let mut buffer = Vec::new();
 
-    String::from_utf8(buffer).unwrap()
+        TextEncoder::new()
+            .encode(&self.registry.gather(), &mut buffer)
+            .unwrap();
+
+        String::from_utf8(buffer).unwrap()
+    }
 }
