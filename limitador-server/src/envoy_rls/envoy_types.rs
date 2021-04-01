17b01abd21eadec07a63566889929e32aa731d53
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-04-01 20:08:05 +0200

    Use #[path] instead of include!() to "include" generated files

--- limitador-server/src/envoy_rls/envoy_types.rs
+++ limitador-server/src/envoy_rls/envoy_types.rs
@@ -4,9 +4,8 @@ pub mod envoy {
     pub mod config {
         #[path = "."]
         pub mod core {
-            pub mod v3 {
-                include!(concat!(env!("OUT_DIR"), "/envoy.config.core.v3.rs"));
-            }
+            #[path = "envoy.config.core.v3.rs"]
+            pub mod v3;
         }
     }
 
@@ -16,30 +15,24 @@ pub mod envoy {
         pub mod common {
             #[path = "."]
             pub mod ratelimit {
-                pub mod v3 {
-                    include!(concat!(
-                        env!("OUT_DIR"),
-                        "/envoy.extensions.common.ratelimit.v3.rs"
-                    ));
-                }
+                #[path = "envoy.extensions.common.ratelimit.v3.rs"]
+                pub mod v3;
             }
         }
     }
 
     #[path = "."]
     pub mod r#type {
-        pub mod v3 {
-            include!(concat!(env!("OUT_DIR"), "/envoy.r#type.v3.rs"));
-        }
+        #[path = "envoy.r#type.v3.rs"]
+        pub mod v3;
     }
 
     #[path = "."]
     pub mod service {
         #[path = "."]
         pub mod ratelimit {
-            pub mod v3 {
-                include!(concat!(env!("OUT_DIR"), "/envoy.service.ratelimit.v3.rs"));
-            }
+            #[path = "envoy.service.ratelimit.v3.rs"]
+            pub mod v3;
         }
     }
 }
