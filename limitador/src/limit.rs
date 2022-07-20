6394e49f66bca7b7bbd441c10db49c412c31a01a
Alex Snaps <alex@wcgw.dev>
2022-07-20 10:54:34 +0100

    Init with defaults when missing

--- limitador/src/limit.rs
+++ limitador/src/limit.rs
@@ -26,10 +26,10 @@ impl From<String> for Namespace {
 #[derive(Eq, Debug, Clone, Serialize, Deserialize)]
 pub struct Limit {
     namespace: Namespace,
-    #[serde(skip_serializing)]
+    #[serde(skip_serializing, default)]
     max_value: i64,
     seconds: u64,
-    #[serde(skip_serializing)]
+    #[serde(skip_serializing, default)]
     name: Option<String>,
 
     // Need to sort to generate the same object when using the JSON as a key or
