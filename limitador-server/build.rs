8062dfb060967cd2a14220a89e06fde810da94ad
Alex Snaps <alex@wcgw.dev>
2022-08-02 10:16:28 -0400

    Check if we have a clean tree

--- limitador-server/build.rs
+++ limitador-server/build.rs
@@ -9,10 +9,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .and_then(|x| String::from_utf8(x.stdout).ok())
         .map(|hash| hash[..8].to_owned());
 
-    println!(
-        "cargo:rustc-env=LIMITADOR_GIT_HASH={}",
-        git_hash.unwrap_or_else(|| "unknown".to_owned())
-    );
+    let dirty = Command::new("git")
+        .args(&["diff", "--stat"])
+        .output()
+        .ok()
+        .filter(|output| output.status.success())
+        .map(|output| !matches!(output.stdout.len(), 0));
+
+    if Some(true) == dirty && git_hash.is_some() {
+        println!(
+            "cargo:rustc-env=LIMITADOR_GIT_HASH={}-dirty",
+            git_hash.unwrap_or_else(|| "unknown".to_owned())
+        );
+    } else {
+        println!(
+            "cargo:rustc-env=LIMITADOR_GIT_HASH={}",
+            git_hash.unwrap_or_else(|| "unknown".to_owned())
+        );
+    }
+
     if let Ok(profile) = std::env::var("PROFILE") {
         println!("cargo:rustc-env=LIMITADOR_PROFILE={}", profile);
     }
