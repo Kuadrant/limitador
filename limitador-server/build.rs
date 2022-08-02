b5f645e791f16e52f14e010f03c1644d7d8311c3
Alex Snaps <alex@wcgw.dev>
2022-08-02 09:58:49 -0400

    Added a more explicit version string and logging it always

--- limitador-server/build.rs
+++ limitador-server/build.rs
@@ -1,4 +1,22 @@
+use std::process::Command;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let git_hash = Command::new("git")
+        .args(&["rev-parse", "HEAD"])
+        .output()
+        .ok()
+        .filter(|output| output.status.success())
+        .and_then(|x| String::from_utf8(x.stdout).ok())
+        .map(|hash| hash[..8].to_owned());
+
+    println!(
+        "cargo:rustc-env=LIMITADOR_GIT_HASH={}",
+        git_hash.unwrap_or_else(|| "unknown".to_owned())
+    );
+    if let Ok(profile) = std::env::var("PROFILE") {
+        println!("cargo:rustc-env=LIMITADOR_PROFILE={}", profile);
+    }
+
     tonic_build::configure()
         .build_server(true)
         .out_dir("src/envoy_rls/protobufs")
