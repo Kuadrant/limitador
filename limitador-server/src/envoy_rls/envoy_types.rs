f7630a3598b66dc4da5f00439ce7a37ff89492d8
Alex Snaps <alex@wcgw.dev>
2022-08-12 15:49:49 -0400

    More fixes

--- limitador-server/src/envoy_rls/envoy_types.rs
+++ limitador-server/src/envoy_rls/envoy_types.rs
@@ -7,7 +7,7 @@ pub mod envoy {
             #[path = "envoy.config.core.v3.rs"]
             // clippy will barf on protobuff generated code for enum variants in
             // v3::socket_option::SocketState, so allow this lint
-            #[allow(clippy::enum_variant_names)]
+            #[allow(clippy::enum_variant_names, clippy::derive_partial_eq_without_eq)]
             pub mod v3;
         }
     }
@@ -19,6 +19,7 @@ pub mod envoy {
             #[path = "."]
             pub mod ratelimit {
                 #[path = "envoy.extensions.common.ratelimit.v3.rs"]
+                #[allow(clippy::derive_partial_eq_without_eq)]
                 pub mod v3;
             }
         }
@@ -27,6 +28,7 @@ pub mod envoy {
     #[path = "."]
     pub mod r#type {
         #[path = "envoy.r#type.v3.rs"]
+        #[allow(clippy::derive_partial_eq_without_eq)]
         pub mod v3;
     }
 
@@ -35,6 +37,7 @@ pub mod envoy {
         #[path = "."]
         pub mod ratelimit {
             #[path = "envoy.service.ratelimit.v3.rs"]
+            #[allow(clippy::derive_partial_eq_without_eq)]
             pub mod v3;
         }
     }
@@ -45,6 +48,7 @@ pub mod xds {
     #[path = "."]
     pub mod core {
         #[path = "xds.core.v3.rs"]
+        #[allow(clippy::derive_partial_eq_without_eq)]
         pub mod v3;
     }
 }
