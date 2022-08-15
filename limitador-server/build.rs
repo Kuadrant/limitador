aa83d91eb6cbdb7513d648588a4a7d95a086e01a
Alex Snaps <alex@wcgw.dev>
2022-08-15 10:46:21 -0400

    refactored build steps in functions

--- limitador-server/build.rs
+++ limitador-server/build.rs
@@ -1,34 +1,13 @@
+use std::error::Error;
 use std::process::Command;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let git_hash = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
-        .output()
-        .ok()
-        .filter(|output| output.status.success())
-        .and_then(|x| String::from_utf8(x.stdout).ok())
-        .map(|hash| hash[..8].to_owned());
+fn main() -> Result<(), Box<dyn Error>> {
+    set_git_hash("LIMITADOR_GIT_HASH");
+    set_profile("LIMITADOR_PROFILE");
+    generate_protobuf()
+}
 
-    let dirty = Command::new("git")
-        .args(&["diff", "--stat"])
-        .output()
-        .ok()
-        .filter(|output| output.status.success())
-        .map(|output| !matches!(output.stdout.len(), 0));
-
-    match git_hash {
-        None => println!("cargo:rustc-env=LIMITADOR_GIT_HASH=unknown"),
-        Some(hash) => match dirty {
-            Some(true) => println!("cargo:rustc-env=LIMITADOR_GIT_HASH={}-dirty", hash),
-            Some(false) => println!("cargo:rustc-env=LIMITADOR_GIT_HASH={}", hash),
-            _ => unreachable!("How can we have a git hash, yet not know if the tree is dirty?"),
-        },
-    }
-
-    if let Ok(profile) = std::env::var("PROFILE") {
-        println!("cargo:rustc-env=LIMITADOR_PROFILE={}", profile);
-    }
-
+fn generate_protobuf() -> Result<(), Box<dyn Error>> {
     tonic_build::configure()
         .build_server(true)
         .out_dir("src/envoy_rls/protobufs")
@@ -41,4 +20,37 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             ],
         )?;
     Ok(())
+}
+
+fn set_profile(env: &str) {
+    if let Ok(profile) = std::env::var("PROFILE") {
+        println!("cargo:rustc-env={}={}", env, profile);
+    }
+}
+
+fn set_git_hash(env: &str) {
+    let git_hash = Command::new("git")
+        .args(&["rev-parse", "HEAD"])
+        .output()
+        .ok()
+        .filter(|output| output.status.success())
+        .and_then(|x| String::from_utf8(x.stdout).ok())
+        .map(|hash| hash[..8].to_owned());
+
+    if let Some(hash) = git_hash {
+        let dirty = Command::new("git")
+            .args(&["diff", "--stat"])
+            .output()
+            .ok()
+            .filter(|output| output.status.success())
+            .map(|output| !matches!(output.stdout.len(), 0));
+
+        match dirty {
+            Some(true) => println!("cargo:rustc-env={}={}-dirty", env, hash),
+            Some(false) => println!("cargo:rustc-env={}={}", env, hash),
+            _ => unreachable!("How can we have a git hash, yet not know if the tree is dirty?"),
+        }
+    } else {
+        println!("cargo:rustc-env={}=unknown", env);
+    }
 }
