588e471fdd83050ef17dcfe1407b1e061cfa899e
Alex Snaps <alex@wcgw.dev>
2022-09-01 17:24:29 -0400

    Testing stricter Limit parsing

--- limitador/src/limit.rs
+++ limitador/src/limit.rs
@@ -50,9 +50,10 @@ impl From<String> for Namespace {
 }
 
 #[derive(Eq, Debug, Clone, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
 pub struct Limit {
     namespace: Namespace,
-    #[serde(skip_serializing, default)]
+    #[serde(skip_serializing)]
     max_value: i64,
     seconds: u64,
     #[serde(skip_serializing, default)]
