f94d36c6793180059530b30028f85727f1a8ca09
Alex Snaps <alex@wcgw.dev>
2023-12-21 11:57:49 -0500

    Added trace spans to all out going redis calls

--- limitador/src/storage/redis/redis_async.rs
+++ limitador/src/storage/redis/redis_async.rs
@@ -13,6 +13,7 @@ use redis::{AsyncCommands, RedisError};
 use std::collections::HashSet;
 use std::str::FromStr;
 use std::time::Duration;
+use tracing::{trace_span, Instrument};
 
 // Note: this implementation does not guarantee exact limits. Ensuring that we
 // never go over the limits would hurt performance. This implementation
@@ -34,9 +35,13 @@ impl AsyncCounterStorage for AsyncRedisStorage {
     async fn is_within_limits(&self, counter: &Counter, delta: i64) -> Result<bool, StorageErr> {
         let mut con = self.conn_manager.clone();
 
-        match con
-            .get::<String, Option<i64>>(key_for_counter(counter))
-            .await?
+        let span = trace_span!("datastore");
+        match async move {
+            con.get::<String, Option<i64>>(key_for_counter(counter))
+                .await
+        }
+        .instrument(span)
+        .await?
         {
             Some(val) => Ok(val - delta >= 0),
             None => Ok(counter.max_value() - delta >= 0),
@@ -47,14 +52,19 @@ impl AsyncCounterStorage for AsyncRedisStorage {
     async fn update_counter(&self, counter: &Counter, delta: i64) -> Result<(), StorageErr> {
         let mut con = self.conn_manager.clone();
 
-        redis::Script::new(SCRIPT_UPDATE_COUNTER)
-            .key(key_for_counter(counter))
-            .key(key_for_counters_of_limit(counter.limit()))
-            .arg(counter.max_value())
-            .arg(counter.seconds())
-            .arg(delta)
-            .invoke_async::<_, _>(&mut con)
-            .await?;
+        let span = trace_span!("datastore");
+        async {
+            redis::Script::new(SCRIPT_UPDATE_COUNTER)
+                .key(key_for_counter(counter))
+                .key(key_for_counters_of_limit(counter.limit()))
+                .arg(counter.max_value())
+                .arg(counter.seconds())
+                .arg(delta)
+                .invoke_async::<_, _>(&mut con)
+                .await
+        }
+        .instrument(span)
+        .await?;
 
         Ok(())
     }
@@ -77,15 +87,27 @@ impl AsyncCounterStorage for AsyncRedisStorage {
                 script_invocation.key(counter_key);
             }
 
-            let script_res: Vec<Option<i64>> = script_invocation.invoke_async(&mut con).await?;
+            let script_res: Vec<Option<i64>> = {
+                let span = trace_span!("datastore");
+                async { script_invocation.invoke_async(&mut con).await }
+                    .instrument(span)
+                    .await?
+            };
             if let Some(res) = is_limited(counters, delta, script_res) {
                 return Ok(res);
             }
         } else {
-            let counter_vals: Vec<Option<i64>> = redis::cmd("MGET")
-                .arg(counter_keys.clone())
-                .query_async(&mut con)
-                .await?;
+            let counter_vals: Vec<Option<i64>> = {
+                let span = trace_span!("datastore");
+                async {
+                    redis::cmd("MGET")
+                        .arg(counter_keys.clone())
+                        .query_async(&mut con)
+                        .await
+                }
+                .instrument(span)
+                .await?
+            };
 
             for (i, counter) in counters.iter().enumerate() {
                 let remaining = counter_vals[i].unwrap_or(counter.max_value()) - delta;
@@ -100,14 +122,19 @@ impl AsyncCounterStorage for AsyncRedisStorage {
         // TODO: this can be optimized by using pipelines with multiple updates
         for (counter_idx, key) in counter_keys.into_iter().enumerate() {
             let counter = &counters[counter_idx];
-            redis::Script::new(SCRIPT_UPDATE_COUNTER)
-                .key(key)
-                .key(key_for_counters_of_limit(counter.limit()))
-                .arg(counter.max_value())
-                .arg(counter.seconds())
-                .arg(delta)
-                .invoke_async::<_, _>(&mut con)
-                .await?;
+            let span = trace_span!("datastore");
+            async {
+                redis::Script::new(SCRIPT_UPDATE_COUNTER)
+                    .key(key)
+                    .key(key_for_counters_of_limit(counter.limit()))
+                    .arg(counter.max_value())
+                    .arg(counter.seconds())
+                    .arg(delta)
+                    .invoke_async::<_, _>(&mut con)
+                    .await
+            }
+            .instrument(span)
+            .await?
         }
 
         Ok(Authorization::Ok)
@@ -120,9 +147,15 @@ impl AsyncCounterStorage for AsyncRedisStorage {
         let mut con = self.conn_manager.clone();
 
         for limit in limits {
-            let counter_keys = con
-                .smembers::<String, HashSet<String>>(key_for_counters_of_limit(&limit))
-                .await?;
+            let counter_keys = {
+                let span = trace_span!("datastore");
+                async {
+                    con.smembers::<String, HashSet<String>>(key_for_counters_of_limit(&limit))
+                        .await
+                }
+                .instrument(span)
+                .await?
+            };
 
             for counter_key in counter_keys {
                 let mut counter: Counter = counter_from_counter_key(&counter_key, &limit);
@@ -134,9 +167,20 @@ impl AsyncCounterStorage for AsyncRedisStorage {
                 // do the "get" + "delete if none" atomically.
                 // This does not cause any bugs, but consumes memory
                 // unnecessarily.
-                if let Some(val) = con.get::<String, Option<i64>>(counter_key.clone()).await? {
+                let option = {
+                    let span = trace_span!("datastore");
+                    async { con.get::<String, Option<i64>>(counter_key.clone()).await }
+                        .instrument(span)
+                        .await?
+                };
+                if let Some(val) = option {
                     counter.set_remaining(val);
-                    let ttl = con.ttl(&counter_key).await?;
+                    let ttl = {
+                        let span = trace_span!("datastore");
+                        async { con.ttl(&counter_key).await }
+                            .instrument(span)
+                            .await?
+                    };
                     counter.set_expires_in(Duration::from_secs(ttl));
 
                     res.insert(counter);
@@ -150,7 +194,10 @@ impl AsyncCounterStorage for AsyncRedisStorage {
     #[tracing::instrument(skip_all)]
     async fn delete_counters(&self, limits: HashSet<Limit>) -> Result<(), StorageErr> {
         for limit in limits {
-            self.delete_counters_associated_with_limit(&limit).await?;
+            let span = trace_span!("datastore");
+            async { self.delete_counters_associated_with_limit(&limit).await }
+                .instrument(span)
+                .await?
         }
         Ok(())
     }
@@ -158,7 +205,10 @@ impl AsyncCounterStorage for AsyncRedisStorage {
     #[tracing::instrument(skip_all)]
     async fn clear(&self) -> Result<(), StorageErr> {
         let mut con = self.conn_manager.clone();
-        redis::cmd("FLUSHDB").query_async(&mut con).await?;
+        let span = trace_span!("datastore");
+        async { redis::cmd("FLUSHDB").query_async(&mut con).await }
+            .instrument(span)
+            .await?;
         Ok(())
     }
 }
@@ -182,12 +232,21 @@ impl AsyncRedisStorage {
     async fn delete_counters_associated_with_limit(&self, limit: &Limit) -> Result<(), StorageErr> {
         let mut con = self.conn_manager.clone();
 
-        let counter_keys = con
-            .smembers::<String, HashSet<String>>(key_for_counters_of_limit(limit))
-            .await?;
+        let counter_keys = {
+            let span = trace_span!("datastore");
+            async {
+                con.smembers::<String, HashSet<String>>(key_for_counters_of_limit(limit))
+                    .await
+            }
+            .instrument(span)
+            .await?
+        };
 
         for counter_key in counter_keys {
-            con.del(counter_key).await?;
+            let span = trace_span!("datastore");
+            async { con.del(counter_key).await }
+                .instrument(span)
+                .await?;
         }
 
         Ok(())
